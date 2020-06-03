b2d0e9b377f5d881cdd1260481504220bf150cca
Diego Camino <diego.camino.r@gmail.com>
2020-06-02 20:32:59 -0500

    Implementing crud expenses

--- routes/expenses.js
+++ routes/expenses.js
@@ -16,6 +16,54 @@ function expensesApi(app) {
       next(err);
     }
   });
+
+  router.get("/:expenseId", async function (req, res, next) {
+    try {
+      const expense = await Promise.resolve(mockDocument[0]);
+      res.status(200).json({
+        data: expense,
+        message: "expense retrieve",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post("/", async function (req, res, next) {
+    try {
+      const createdExpenseId = await Promise.resolve(mockDocument[0]._id);
+      res.status(201).json({
+        data: createdExpenseId,
+        message: "expense created",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put("/:expenseId", async function (req, res, next) {
+    try {
+      const updatedExpenseId = await Promise.resolve(mockDocument[0]._id);
+      res.status(200).json({
+        data: updatedExpenseId,
+        message: "expense updated",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete("/:expenseId", async function (req, res, next) {
+    try {
+      const deletedExpenseId = await Promise.resolve(mockDocument[0]._id);
+      res.status(200).json({
+        data: deletedExpenseId,
+        message: "expense deleted",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 }
 
 module.exports = expensesApi;
